51ff4b72dcb25b124cf2fd8176c039bdf20f8472
sri209 <kondasri98@yahoo.com>
2020-04-02 17:35:42 +0530

    added firebase files

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+ 
+  color: Colors.black54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.black26,
+
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.white70,
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
---
+++ lib/utilities/styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+final kBackgroundColor1 = [
+  //Color(0xFF5099F9),
+  Colors.blueAccent,
+  Color(0xFF4563DB),
+  Color(0xFF5036D5),
+  Colors.purple.shade700,
+];
+
+final kBackgroundColor = [
+  //Color(0xFF5099F9),
+  Colors.white,
+  Colors.white,
+  Colors.white,
+  Colors.white,
+];
+
+final Color titleColor = Colors.black87;
+final Color subTitleColor = Color(0XFF1F4B6E).withOpacity(0.9);
+
+final Color descriptionColor = Colors.black38;
+final Color optionColor = Colors.black;
+
+
+final kSeeMoreStyle = TextStyle(
+  color: descriptionColor,
+  fontFamily: 'Quicksand',
+  fontSize: 16.0,
+  fontWeight: FontWeight.w700,
+  height: 1.5,
+);
+
+final kSubtitleStyle = TextStyle(
+  color: subTitleColor,
+  fontFamily: 'Quicksand',
+  fontWeight: FontWeight.w700,
+  fontSize: 18.0,
+  height: 1.2,
+);
+
+final kDescriptionStyle = TextStyle(
+  color: descriptionColor,
+  fontFamily: 'Quicksand',
+  fontWeight: FontWeight.w500,
+  fontSize: 18.0,
+  height: 1.5,
+);
+final kOptionsStyle = TextStyle(
+  color: optionColor,
+  fontFamily: 'Quicksand',
+  fontWeight: FontWeight.w600,
+  fontSize: 18.0,
+  height: 1.2,
+);
+
+
+final kHeadingStyle = TextStyle(
+  //color: kBackgroundColor1,
+  fontFamily: 'Quicksand',
+  fontSize: 30.0,
+  fontWeight: FontWeight.w700,
+  height: 1.5,
+);
+
+
