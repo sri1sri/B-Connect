ac73e16595ceeb058a943817249421705f1555d9
Huy <huynn109@gmail.com>
2020-05-18 13:24:01 +0700

    Fixed method channel flutter for Android

--- android/app/src/main/kotlin/com/bhavaniconnect/bhavani_connect/MainActivity.kt
+++ android/app/src/main/kotlin/com/bhavaniconnect/bhavani_connect/MainActivity.kt
@@ -3,10 +3,19 @@ package com.bhavaniconnect.bhavani_connect
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity(), PluginRegistry.PluginRegistrantCallback {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        flutterEngine.plugins.add(FirebaseMessagingPlugin());
     }
+
+    override fun registerWith(registry: PluginRegistry) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
+    }
+
 }
